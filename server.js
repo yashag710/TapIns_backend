653a119bcc7894b72df1383dcdb9affdc9a975bd
yashag710 <yashag1810@gmail.com>
2025-07-20 02:22:03 +0530

    Changes10

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ require("dotenv").config();
 // Middleware
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://tap-ins.vercel.app',
     credentials: true
 }));
 app.use(cookieParser());
